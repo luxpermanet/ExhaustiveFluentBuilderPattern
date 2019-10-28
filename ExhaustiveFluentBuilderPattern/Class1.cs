e7699c122d79abbd877c481f369b15d913c769ca
ERGO <ergo@simcorp.com>
2019-10-28 16:03:27 +0100

    Add project files.

---
+++ ExhaustiveFluentBuilderPattern/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExhaustiveFluentBuilderPattern
+{
+    public class Class1
+    {
+    }
+}
